5d160377be624d49887dcc2c645b7442085b9a9f
realzaidiguy <realzaidiguy@gmail.com>
2017-01-24 18:21:25 -0500

    wrote schema and seeds sql files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- work on the burgers_db
+USE `burgers_db`;
+
+-- insert 3 burgers
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Fish Fillet', '0', CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Roast Beef', '0', CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Quarter Pounder', '0', CURRENT_TIMESTAMP);
